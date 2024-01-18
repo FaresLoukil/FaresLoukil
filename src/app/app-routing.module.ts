4e95cba9b2d2de3c2ac3819aae42e540ab2092f6
Fares LOUKIL <floukil@vermeg.com>
2024-01-18 15:28:04 +0100

    up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [];
-
+RouterModule.forRoot(routes, {
+  useHash: true
+  // or set baseHref if not using hash routing
+})
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
